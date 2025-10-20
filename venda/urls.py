bf546587f7295cb635c4519c4c5c7bd3b7891391
rafaelafariasm56-blip <rafaelafariasm56@gmail.com>
2025-10-20 04:32:41 -0300

    Update urls.py

--- venda/urls.py
+++ venda/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from core.views import APIRootView
-from users.views import RegisterView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 schema_view = get_schema_view(
@@ -34,17 +33,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("", include("users.urls")),
     path("admin/", admin.site.urls),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
-    path("api/auth/", include("users.urls")),
-    path("api/cardapio/", include("cardapio.urls")),
+    path("api/users/", include("users.urls")),      
+    path("api/core/", include("core.urls")),        
     path("api/pedidos/", include("pedidos.urls")),
-    path("api/pagamento/", include("pagamento.urls")),
-
-    # Swagger
-    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
